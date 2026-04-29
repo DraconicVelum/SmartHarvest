05b4ba1d9b3eaa77863bd1d4863d674f6992b4e4
Draconic Velum <87547775+DraconicVelum@users.noreply.github.com>
2026-04-29 11:31:00 +0100

    Enhance BlockPlaceListener to handle instant break crops
    Added support for instant break crops in BlockPlaceListener.

--- src/main/java/com/draconicvelum/SmartHarvest/listener/BlockPlaceListener.java
+++ src/main/java/com/draconicvelum/SmartHarvest/listener/BlockPlaceListener.java
@@ -3,6 +3,7 @@ package com.draconicvelum.SmartHarvest.listener;
 import com.draconicvelum.SmartHarvest.SmartHarvestPlugin;
 import com.draconicvelum.SmartHarvest.manager.CoreProtectHook;
 import com.draconicvelum.SmartHarvest.manager.CropManager;
+
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,15 +15,14 @@ public class BlockPlaceListener implements Listener {
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent event) {
 
-        // If CoreProtect is active → DO NOTHING
         if (CoreProtectHook.isEnabled()) return;
 
         Material type = event.getBlock().getType();
 
-        if (!CropManager.isVerticalCrop(type)) return;
+        if (!CropManager.isVerticalCrop(type) && !CropManager.isInstantBreakCrop(type)) return;
 
         event.getBlock().setMetadata(
-                "placed_crop",
+                SmartHarvestPlugin.PLACED_KEY.getKey(),
                 new FixedMetadataValue(SmartHarvestPlugin.getInstance(), true)
         );
     }
